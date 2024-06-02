f85e1e8f120e753e9ac6fddb18db2f9d6c08f20c
ved prakash pathak <156571640+Pathak-jii2407@users.noreply.github.com>
2024-06-02 14:15:59 +0530

    Update urls.py

--- NTI/base/urls.py
+++ NTI/base/urls.py
@@ -6,12 +6,12 @@ from . import views
 urlpatterns = [
     path("", views.index, name='index'),
     path("status/", views.status, name='status'),
-    path("student-account/<str:username>/<str:password>/", views.student_account, name='student-account'),
+    path('student-account/', views.student_account, name='student_account'),
     path("teacher-account/", views.teacher_account, name='teacher-account'),
-    path("search-account/", views.search_account, name='search-account'),
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
     path('teacher/', views.teacher_login, name='teacher'),
     path('signout/', views.signout, name='signout'),
     path('edit/', views.edit, name='edit'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
