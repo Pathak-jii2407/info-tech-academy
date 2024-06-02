bda95b96b3a77f0404cbdf28baba8920f26ff3d9
ved prakash pathak <156571640+Pathak-jii2407@users.noreply.github.com>
2024-06-02 14:30:15 +0530

    Update settings.py

--- NTI/NTI/settings.py
+++ NTI/NTI/settings.py
@@ -116,6 +116,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
