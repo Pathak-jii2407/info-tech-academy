b74ef1cd7c05904db0a4672824329633f0c52cf4
ved prakash pathak <156571640+Pathak-jii2407@users.noreply.github.com>
2024-06-02 14:15:35 +0530

    Update views.py

--- NTI/base/views.py
+++ NTI/base/views.py
@@ -6,51 +6,88 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from .sendmail import send
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 
 
 
 def index(request):
     today = date.today()
     present_students = StudentStatus.objects.filter(status_date=today, status='Present').values_list('name', flat=True)
-    present = {'present_student': present_students}
-    return render(request, 'base/NTI-Tech-Academy.html', present)
+    teachers = TeacherAccount.objects.all()
+    if User is not None:
+        context = {
+            'present_student': present_students,
+            'teachers': teachers,
+            'user': request.user,
+        }
+    try:
+        try:
+            teacher_data = TeacherAccount.objects.get(username=request.user)
+            context['data']=teacher_data
+        except:
+            student_data = StudentInfo.objects.get(username=request.user)
+            context['data']=student_data
+    except:
+        pass
 
+    return render(request, 'base/NTI-Tech-Academy.html', context)
 
 def teacher_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        teacher = TeacherAccount.objects.filter(username=username, password=password).first()
         
-        if teacher is not None:
-            return redirect('status')
-        else:
-            return render(request, 'base/teacher-signin.html', {'message': 'Not found as a teacher'})
-            
+        try:
+            teacher = TeacherAccount.objects.get(username=username)
+        except TeacherAccount.DoesNotExist:
+            teacher = None
+        
+        if teacher:
+            if not User.objects.filter(username=username).exists():
+                user = User.objects.create_user(username=username, password=password)
+                user.is_active = True
+                user.is_staff = True
+                user.is_superuser = True
+                user.save()
+            else:
+                user = authenticate(username=username, password=password)
+            if user:
+                login(request, user) 
+                return redirect('index')
+            else:
+                return render(request, 'base/teacher-signin.html', {'message': 'Not found as a teacher or incorrect password'})
     return render(request, 'base/teacher-signin.html')
 
 
+@staff_member_required
 def status(request):
     if request.method == 'POST':
         present_students = request.POST.getlist('attendance[]')
         today = date.today()
-        
-        for student_id in present_students:
-            student = StudentInfo.objects.get(id=student_id)
+
+        all_students = StudentInfo.objects.all()
+
+        for student in all_students:
+            if str(student.id) in present_students:
+                status = 'Present'
+
+            
             student_status, created = StudentStatus.objects.get_or_create(
                 name=student.name,
                 phone_num=student.phone_num,
                 status_date=today,
-                defaults={'status': 'Present'}
+                defaults={'status': status}
             )
             if not created:
-                student_status.status = 'Present'
+                student_status.status = status
                 student_status.save()
-        
+
         return redirect('index')
 
     students = StudentInfo.objects.all()
-    return render(request, 'base/NTI-Tech-status.html', {'students': students})
+    return render(request, 'base/NTI-Tech-status.html', {'students': students, 'user': request.user})
+
+
 
 def signup(request):
     if request.method == 'POST':
@@ -75,10 +112,9 @@ def signup(request):
             
             
             myuser = User.objects.create_user(username, email, password)
-            myuser.first_name = firstname
-            myuser.last_name = lastname
+            myuser.first_name = firstname.capitalize()
+            myuser.last_name = lastname.capitalize()
             
-
             student_data = StudentInfo(
                 name=f'{firstname} {lastname}',
                 phone_num=phone_num,
@@ -89,7 +125,8 @@ def signup(request):
             myuser.save()
             student_data.save()
 
-            # send(user_email=email, first_name=firstname)
+            send(user_email=email, first_name=firstname)
+            user = authenticate(username=username, password=password)
             return redirect('signin')
         except Exception as e:
             messages.error(request, f"Error: {e}")
@@ -104,33 +141,28 @@ def signin(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            return student_account(request,username,password)
+            return student_account(request)
+        
+        
         else:
             messages.error(request, 'Invalid username or password')
     return render(request, 'base/signin.html')
+
+def student_account(request):
+    try:
+        student = StudentInfo.objects.get(username=request.user.username)
+        data = Edit.objects.filter(student_info=student).last()
+        return render(request, 'base/NTI-Tech-Academy-student-Account.html', {'student': student, 'data': data})
+
+    except StudentInfo.DoesNotExist:
+        pass
+
+    return redirect('signin')
 
 def signout(request):
     logout(request)
     messages.success(request, 'Successfully logged out')
     return redirect('index')
-
-def student_account(request, username=None, password=None):
-    try:
-        if request.method == 'POST' and username and password:
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                student = StudentInfo.objects.filter(username=username, password=password).first()
-                if student:
-                    data = Edit.objects.filter(student_info=student).first()
-                    return render(request, 'base/NTI-Tech-Academy-student-Account.html', {'student': student, 'data': data})
-        
-        return redirect('signin')
-    except:
-        return redirect('signin')
-
-
-
 
 @login_required
 def edit(request):
@@ -138,27 +170,19 @@ def edit(request):
         if 'image' in request.FILES and 'subject' in request.POST:
             image = request.FILES['image']
             subject = request.POST['subject']
-            username = request.user.username  # Accessing the username of the currently authenticated user
-            studentinfo = StudentInfo.objects.get(username=username)
-            Edit.objects.create(student_info=studentinfo, image=image, subject=subject)
-            return redirect('index')
+            student = StudentInfo.objects.get(username=request.user.username)
+            data = Edit.objects.create(student_info=student, image=image, subject=subject)
+            return render(request, 'base/NTI-Tech-Academy-student-Account.html', {'student': student, 'data': data})
     return redirect('signin')
-
-
 
 def teacher_account(request):
     teachers = TeacherAccount.objects.all()
     return render(request, 'base/NTI-Tech-Academy-teacher-Account.html', {'teachers': teachers})
 
 
-def search_account(request):
-    query = request.GET.get('query', '').strip()
+
+def dashboard(request):
+    present_data = StudentStatus.objects.all()
+    return render(request, 'base/dashboard.html', {'present': present_data})
+
     
-    students = []
-    teachers = []
-    
-    if query:
-        students = StudentInfo.objects.filter(name__icontains=query)
-        teachers = TeacherAccount.objects.filter(name__icontains=query)
-        
-    return render(request, 'base/search_account.html', {'students': students, 'teachers': teachers, 'query': query})
