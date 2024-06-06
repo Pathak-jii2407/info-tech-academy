230cfbaa14611e5991e52d00eb3478fbc9249a5b
ved prakash pathak <156571640+Pathak-jii2407@users.noreply.github.com>
2024-06-06 10:22:14 +0530

    Update models.py

--- NTI/base/models.py
+++ NTI/base/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
 
+
 class StudentStatus(models.Model):
     id = models.AutoField(primary_key=True, serialize=False)
     name = models.CharField(max_length=40)
     phone_num = models.BigIntegerField()
-    status = models.CharField(max_length=100)  
+    status = models.CharField(max_length=100)
     status_date = models.DateField()
 
     def __str__(self):
         return f"{self.name} - {self.status}"
+
 
 class StudentInfo(models.Model):
     id = models.AutoField(primary_key=True, serialize=False)
